da298bd3e185fcf543952d5fa46563f833257381
BigNews3 <Precurs@r313>
2021-02-05 16:33:15 +0100

    add books routes

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ import sys, os
 import json
 from flask import Flask
 from flask import render_template
+from flask import jsonify, make_response
 
 app = Flask(__name__)
 
@@ -16,6 +17,34 @@ def index():
 def hello(name=None):
     return render_template('hello.html', name=name)
 
+
+@app.route("/api/books")
+def books():
+    with open('db/books.json') as f:
+        data = json.load(f)
+    return jsonify(data)
+
+
+@app.route('/api/books/<int:book_id>')
+def book_by_id(book_id):
+    with open('db/books.json') as f:
+        data = json.load(f)
+
+    for book in data:
+        if 'isbn' in book and book['isbn'] == str(book_id):
+            return jsonify(book)
+        return 'null'
+
+
+@app.route('/api/books/title/<string:title>')
+def book_by_title(title):
+    with open('db/books.json') as f:
+        data = json.load(f)
+
+    for book in data:
+        if 'title' in book and book['title'] == title:
+            return jsonify(book)
+        return 'null'
 
 
 
