6a50bf67e6df1918c05a519d154cb360f4b113cd
BigNews3 <Precurs@r313>
2021-02-18 10:46:10 +0100

    jour 2 mysql

--- jour2-docker-mysql/app/app.py
+++ jour2-docker-mysql/app/app.py
@@ -1,26 +1,17 @@
 # compose_flask/app.py
 from flask import Flask
-from redis import Redis
 import mysql.connector
 import sqlalchemy
 from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    mydb = mysql.connector.connect(
-        host="db",
-        user="compta",
-        password="password",
-        database='classicmodels'
-    )
-    print(mydb)
+    engine = create_engine('mysql+pymysql://compta:password@db:3306/classicmodels')
 
-    engine = create_engine('mysql://compta:password@db:3306/classicmodels')
-
-
-    return ' - - - great has viewed {} time(s) - - -'.format('coucou')
+    return ' - - - MySQL Database `classicmodels` connection ok - - -'
 
 
 if __name__ == "__main__":
